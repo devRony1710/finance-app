adb03ba0addbcc96610f556e858d1e63dc324b74
Rony Antolinez <rony1710.dev@gmail.com>
2025-10-27 21:28:05 -0500

    wip: add test for onClick event in button

--- src/components/button/button.test.jsx
+++ src/components/button/button.test.jsx
@@ -2,9 +2,11 @@ import { render, screen } from '@testing-library/react'
 import { describe, it, expect, beforeEach } from 'vitest'
 import { Button } from './button'
 
+const mockFnClick = vi.fn()
+
 describe('Button', () => {
   beforeEach(() => {
-    render(<Button label="Button" variant="primary" size="medium" />)
+    render(<Button label="Button" variant="primary" size="medium" onClick={mockFnClick} />)
   })
 
   it('should render with default props', () => {
@@ -17,5 +19,11 @@ describe('Button', () => {
     expect(button).toHaveTextContent('Button')
     expect(button).toHaveClass('bg-primary text-white')
     expect(button).toHaveClass('px-4 py-2 text-base w-48')
+  })
+
+  it('should call onClick when clicked', () => {
+    const button = screen.getByRole('button')
+    button.click()
+    expect(mockFnClick).toHaveBeenCalledTimes(1)
   })
 })
