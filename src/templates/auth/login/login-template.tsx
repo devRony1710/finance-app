bb74f7b9dc6c85f695141da5a900d392656fea0d
Rony Antolinez <rony1710.dev@gmail.com>
2025-10-27 22:37:42 -0500

    change: styles for desktop view

--- src/templates/auth/login/login-template.tsx
+++ src/templates/auth/login/login-template.tsx
@@ -5,11 +5,12 @@ import FinanceWallet from '@/assets/finance-wallet.png'
 
 export const LoginTemplate = () => {
   return (
-    <section className="w-full h-screen p-4 flex flex-col gap-4 items-center">
+    <section className="w-full h-screen lg:h-[90vh] max-w-[980px] mx-auto p-4 flex flex-col gap-4 items-center lg:border-2 border-primary rounded-lg lg:mt-10 lg:shadow-lg">
       <div className="w-[220px] flex items-center justify-center h-[220px] rounded-full border-2 border-primary bg-primary">
         <img src={FinanceWallet} alt="FinanceWallet" className="w-auto object-cover h-[200px]" />
       </div>
-      <div className="flex items-center justify-start w-full gap-2">
+
+      <div className="flex items-center justify-start lg:justify-center w-full gap-2">
         <div className="w-12 h-12 rounded-md bg-primary flex items-center justify-center">
           <Wallet className="w-8 h-8 text-white" />
         </div>
@@ -40,13 +41,16 @@ export const LoginTemplate = () => {
         />
       </form>
 
-      <div className="w-full flex justify-end pr-2">
+      <div className="w-full flex justify-end lg:justify-center pr-2">
         <a className="text-blue-500 text-sm font-medium underline underline-offset-4" href="/register">
           ¿Olvidaste tu contraseña?
         </a>
       </div>
 
-      <Button size="large" label="Iniciar sesión" />
+      <div className="w-full flex justify-center lg:w-[20rem]">
+        <Button size="large" label="Iniciar sesión" />
+      </div>
+
       <div className="w-full flex justify-center">
         <a className="text-gray-500 text-sm font-medium underline underline-offset-4" href="/register">
           ¿No tienes una cuenta? <span className="font-bold text-blue-500">Registrate</span>
