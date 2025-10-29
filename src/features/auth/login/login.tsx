ccf05ad4d51001a349f3c5d1c36681716f2265c5
Rony Antolinez <rony1710.dev@gmail.com>
2025-10-29 09:41:11 -0500

    fix: add loading state in login module

--- src/features/auth/login/login.tsx
+++ src/features/auth/login/login.tsx
@@ -5,13 +5,14 @@ import { useMutation } from '@tanstack/react-query'
 import { useNavigation } from '@/hooks/use-navigation/use-navigation'
 import toast from 'react-hot-toast'
 import { handleLoginErrors } from '@/lib/handle-request-errors/handle-login-errors/handle-login-errors'
+import { Loader } from 'lucide-react'
 
 export const Login = () => {
   const { control, watch, errors, isSubmitting } = useLogin()
   const { loginTrigger } = useAuth()
   const { navigateTo } = useNavigation()
 
-  const { mutateAsync } = useMutation({
+  const { mutateAsync, isPending } = useMutation({
     mutationFn: () => loginTrigger(watch('email'), watch('password')),
     onSuccess: () => {
       navigateTo('/dashboard')
@@ -21,6 +22,14 @@ export const Login = () => {
       toast.error(handleLoginErrors(error.message))
     },
   })
+
+  if (isPending) {
+    return (
+      <section className="w-full h-screen flex items-center justify-center">
+        <Loader className="w-12 h-12 animate-spin" />
+      </section>
+    )
+  }
 
   return <LoginTemplate config={{ control, errors, isSubmitting }} handleLogin={mutateAsync} />
 }
