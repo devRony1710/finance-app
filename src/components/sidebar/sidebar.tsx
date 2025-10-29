a3a76c879ba4848f1a3a540aa15944c93616f121
Rony Antolinez <rony1710.dev@gmail.com>
2025-10-29 03:07:36 -0500

    wip: add logout fn

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,6 +1,8 @@
 import clsx from 'clsx'
 import { useSidebar } from './_logic/use-sidebar'
 import { Link } from 'react-router-dom'
+import { useAuth } from '@/context/auth-context/auth-context'
+import { Button } from '../button/button'
 
 const SidebarItem = ({
   name,
@@ -30,13 +32,14 @@ const SidebarItem = ({
 
 export const Sidebar = () => {
   const { SidebarOptions } = useSidebar()
+  const { logout } = useAuth()
 
   const isActive = (path: string) => {
     return window.location.pathname === path
   }
 
   return (
-    <section className="w-full h-full flex flex-col gap-2">
+    <section className="w-full h-full flex flex-col gap-2 relative">
       {SidebarOptions.map((option, index) => (
         <SidebarItem
           key={index}
@@ -46,6 +49,10 @@ export const Sidebar = () => {
           path={option.path}
         />
       ))}
+
+      <div className="w-full flex items-center justify-center absolute bottom-0">
+        <Button onClick={logout} label="Cerrar sesión" size="medium" variant="primary" />
+      </div>
     </section>
   )
 }
